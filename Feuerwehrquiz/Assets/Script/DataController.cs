415a793376bd72c6010f85f6d63f18ad1240456e
Maynor Gustavo <uk004992@student.uni-kassel.de>
2021-02-07 00:05:06 +0100

    Start Project - Menu Screen added

---
+++ Feuerwehrquiz/Assets/Script/DataController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+}
---
+++ Feuerwehrquiz/Assets/Script/MenuScreenController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreenController : MonoBehaviour
+{
+    //Panels
+    public GameObject StartPanel;
+    public GameObject PanelName;
+    public GameObject PanelList;
+
+    //start the next Scene
+    public void StartPanelName()
+    {
+        if (PanelName != null)
+        {
+            PanelName.SetActive(true);
+            StartPanel.SetActive(false);
+        }
+    }
+
+    public void StartPanelList()
+    {
+        if (PanelList != null)
+        {
+            PanelList.SetActive(true);
+            StartPanel.SetActive(false);
+        }
+    }
+
+    public void BackToStart()
+    {
+        if (StartPanel != null)
+        {
+            StartPanel.SetActive(true);
+        }
+        PanelName.SetActive(false);
+        PanelList.SetActive(false);
+
+    }
+
+    public void StartQuiz()
+    {
+        Debug.Log("Game is Started");
+    }
+}
